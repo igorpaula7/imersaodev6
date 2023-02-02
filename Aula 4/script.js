b966b3ed43eda0cbbef077a61f10b36f35525529
Igor de Paula <igor.paula7@outlook.com>
2023-02-02 11:35:57 -0300

    Aula 04 v2

--- Aula 4/script.js
+++ Aula 4/script.js
@@ -4,11 +4,18 @@ function adicionarFilme() {
     
     var elementoListaFilmes = document.getElementById("listaFilmes")
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + `
-    <div class="itemlista">
+    <div  class="itemlista">
     <img src="${filmeFavorito}" class="imagemfilme">
     <p class="page-subtitle">${nomeFilme}</p>
     </div>
     `
     document.getElementById('filme').value = ''
     document.getElementById('nome').value = ''
+  }
+
+  function limparFilmes() {
+    var removerListaFilmes = document.getElementsByClassName('itemlista');
+    for(var i = removerListaFilmes.length - 1; i >= 0; i--){
+    removerListaFilmes[i].remove()
+    }
   }
